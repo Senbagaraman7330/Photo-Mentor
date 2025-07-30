b65b8f9551307e84c815d3bf433a4e24f7608e0c
Senbagaraman7330 <senbagaraman7330@gmail.com>
2025-07-30 10:18:56 +0530

    I could create Basic site using Bootstrap frame work

---
+++ __-index.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <header>
+        <div>
+            <div class="">
+
+                <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+
+                    <a href="__-index.php" class="navbar-brand ms-4 text-success ">
+                        <h1 class="h1">Photo Mentor</h1>
+                    </a>
+
+                    <button class="navbar-toggler me-4 text-center" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarcollapse">
+                        <span class="navbar-toggler-icon"></span></button>
+
+                    <div class=" navbar-collapse collapse nav d-flex-inline  justify-content-md-end"
+                        id="navbarcollapse">
+
+                        <ul
+                            class="d-flex flex-row flex-lg-row flex-sm-column  justify-content-evenly justify-content-sm-start  me-4">
+
+                            <li class="nav-item">
+                                <a href="__-index.php" class="nav-link text-success">Home</a>
+                            </li>
+
+
+
+                            <li class="nav-item">
+                                <a href="__-sign-in.php" class="nav-link text-success">Sign In</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="__-sign-up.php" class="nav-link text-success">Sign Up</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-success">About</a>
+                            </li>
+
+                            <li class="nav-item">
+
+                                <div class="dropdown-center ">
+
+                                    <button class="btn btn-success dropdown-toggle" type="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">Hit Me</button>
+
+                                    <div class="dropdown-menu ">
+
+                                        <span class="dropdown-item">Help</span>
+                                        <span class="dropdown-item">Contact</span>
+
+                                    </div>
+
+                            </li>
+
+
+
+
+                        </ul>
+                    </div>
+
+
+                </nav>
+
+            </div>
+
+        </div>
+    </header>
+
+    <main>
+        <!-- row 1  -->
+
+        <div class="container-fluid my-4">
+            <div class="row">
+
+                <!-- Card 1 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- Card 2 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <!-- Card 3 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- row 2  -->
+        <div class="container-fluid my-4">
+            <div class="row">
+                <!-- Card 1 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 2 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 3 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- row 3  -->
+        <div class="container-fluid my-4">
+            <div class="row">
+                <!-- Card 1 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 2 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 3 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://picsum.photos/300/200" alt="Sample Image" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Posted Image</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+                                placeat quia, alias dolore ex quae. Debitis omnis et perferendis laborum reprehenderit
+                                iusto nam quisquam aliquid, mollitia tempore corrupti dolor corporis?
+                            </p>
+                        </div>
+
+                        <div class="container-fluid d-flex ">
+                            <div class="btn-group m-3 me-auto">
+                                <button class="btn btn-outline-success">Like</button>
+                                <button class="btn btn-outline-success">Dislike</button>
+                            </div>
+
+                            <p class="m-3 text-center">10 min ago</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </main>
+
+
+    <footer class="text-body-secondary py-5 bg-dark">
+        <div class="container  text-success">
+            <p class="float-end mb-1 "> <a href="#" class="text-success">Back to top</a> </p>
+            <p class="mb-1 h3">Thank you for visited the Wed site</p>
+            <p>By <strong>Senbaga Raman</strong></p>
+        </div>
+    </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ __-sign-in.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<style>
+html,
+body {
+    height: 100%;
+}
+
+body {
+    display: flex;
+    flex-direction: column;
+}
+
+main {
+    flex: 1;
+}
+</style>
+
+<body>
+
+     <header>
+        <div>
+            <div class="">
+
+                <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+
+                    <a href="__-index.php" class="navbar-brand ms-4 text-success ">
+                        <h1 class="h1">Photo Mentor</h1>
+                    </a>
+
+                    <button class="navbar-toggler me-4 text-center" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarcollapse">
+                        <span class="navbar-toggler-icon"></span></button>
+
+                    <div class=" navbar-collapse collapse nav d-flex-inline  justify-content-md-end"
+                        id="navbarcollapse">
+
+                        <ul
+                            class="d-flex flex-row flex-lg-row flex-sm-column  justify-content-evenly justify-content-sm-start  me-4">
+
+                            <li class="nav-item">
+                                <a href="__-index.php" class="nav-link text-success">Home</a>
+                            </li>
+
+
+
+                            <li class="nav-item">
+                                <a href="__-sign-in.php" class="nav-link text-success">Sign In</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="__-sign-up.php" class="nav-link text-success">Sign Up</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link text-success">About</a>
+                            </li>
+
+                            <li class="nav-item">
+
+                                <div class="dropdown-center ">
+
+                                    <button class="btn btn-success dropdown-toggle" type="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">Hit Me</button>
+
+                                    <div class="dropdown-menu ">
+
+                                        <span class="dropdown-item">Help</span>
+                                        <span class="dropdown-item">Contact</span>
+
+                                    </div>
+
+                            </li>
+
+
+
+
+                        </ul>
+                    </div>
+
+
+                </nav>
+
+            </div>
+
+        </div>
+    </header>
+
+    <main>
+        <div class="container mt-4 ">
+
+            <div class="card w-75 mx-auto border-3">
+                <div class="card-body">
+                    <form action="">
+                        <h1 class="text-center fw-bold text-success my-4">Sign In</h1>
+
+                        <div class="form-floating my-4">
+                            <input type="email" class="form-control " id="emailinput" placeholder="Email" required>
+                            <label for="emailinput" class="form-label ">Email</label>
+
+                        </div>
+
+                        <div class="form-floating  my-4">
+                            <input type="password" class="form-control " id="passwordinput" placeholder="Password"
+                                required>
+                            <label for="passwordinput" class="form-label ">Password</label>
+                        </div>
+
+                        <div class="form-text text-center  mt-4">Create account <a href="__-sign-up.php"
+                                style="text-decoration: none;">Sign Up</a> ?</div>
+
+                        <div class="d-flex justify-content-center  my-4">
+                            <button class="btn btn-outline-success mt-3 px-5 " type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+   <footer class="text-body-secondary py-5 bg-dark">
+        <div class="container  text-success">
+            <p class="float-end mb-1 "> <a href="#" class="text-success">Back to top</a> </p>
+            <p class="mb-1 h3">Thank you for visited the Wed site</p>
+            <p>By <strong>Senbaga Raman</strong></p>
+        </div>
+    </footer>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
